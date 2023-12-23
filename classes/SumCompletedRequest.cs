462ebd125f1a588c814e3fb41fb9235d679f490e
Николай Кошенский <nkot81088@gmail.com>
2023-12-23 18:06:42 +0300

    Добавил класс с расчетом выполненых заявок

---
+++ classes/SumCompletedRequest.cs
@@ -0,0 +1,28 @@
+﻿using PP_PM02_Koshenskiy.database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_PM02_Koshenskiy.classes
+{
+    internal class SumCompletedRequest
+    {
+        public int Sum()
+        {
+            int sumCompletedRequest = 0;
+            List<Accounting> accountings = AppModel.dbModel.Accounting.ToList();
+
+            foreach (Accounting accounting in accountings)
+            {
+                if (accounting.status_id == 6)
+                {
+                    sumCompletedRequest++;
+                }
+            }
+
+            return sumCompletedRequest;
+        }
+    }
+}
